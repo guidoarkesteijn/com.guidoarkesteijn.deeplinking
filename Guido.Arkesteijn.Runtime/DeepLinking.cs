9fd6a92014eb360ccdc11b5b0fd7e39f1814cc74
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2019-08-03 16:13:03 +0200

    Added correct meta files and working on simple implementation

---
+++ Guido.Arkesteijn.Runtime/DeepLinking.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Guido.Arkesteijn.Runtime
+{
+    public class DeepLinking
+    {
+        public static bool Initialized { get { return Instance != null; } }
+        public static DeepLinking Instance { get; private set; }
+
+        public delegate void DeepLinkingTriggerHandler(string key, Dictionary<string, object> pairs);
+
+        private Dictionary<string, List<DeepLinkingTriggerHandler>> dictionary = new Dictionary<string, List<DeepLinkingTriggerHandler>>();
+        private DeepLinkingSettings settings;
+
+        public static void Initialize()
+        {
+            Initialize(DeepLinkingSettings.DefaultSettings);
+        }
+
+        public static void Initialize(DeepLinkingSettings settings)
+        {
+            if(Initialized)
+            {
+                return;
+            }
+
+            Instance = new DeepLinking();
+            Instance.settings = settings;
+        }
+        
+        public void Subscribe(string key, DeepLinkingTriggerHandler handler)
+        {
+            if(dictionary.ContainsKey(key))
+            {
+                dictionary[key].Add(handler);
+            }
+            else
+            {
+                dictionary.Add(key, new List<DeepLinkingTriggerHandler>() { handler });
+            }
+        }
+
+        public void Unsubscribe(string key, DeepLinkingTriggerHandler handler)
+        {
+            if(dictionary.ContainsKey(key))
+            {
+                dictionary[key].Remove(handler);
+            }
+        }
+
+        public void Trigger(string key, Dictionary<string, object> payLoad)
+        {
+            if(dictionary.ContainsKey(key))
+            {
+                foreach (var item in dictionary[key])
+                {
+                    item(key, payLoad);
+                }
+            }
+        }
+    }
+}
---
+++ Guido.Arkesteijn.Runtime/DeepLinkingSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Guido.Arkesteijn.Runtime
+{
+    [CreateAssetMenu(menuName = "Guido Arkesteijn/Deep Linking", fileName = nameof(DeepLinkingSettings))]
+    public class DeepLinkingSettings : ScriptableObject
+    {
+        public static DeepLinkingSettings DefaultSettings
+        {
+            get
+            {
+                DeepLinkingSettings defaultSettings = CreateInstance(typeof(DeepLinkingSettings)) as DeepLinkingSettings;
+                return defaultSettings;
+            }
+        }
+
+        public string Domain { get { return domain; } }
+        [SerializeField] private string domain;
+    }
+}
